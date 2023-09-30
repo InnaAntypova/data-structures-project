27d2f69c21553ab7ecbfecb1059a06a1e42c5c89
Inna Antypova <nightallatiel@gmail.com>
2023-09-30 16:49:55 +0300

    add tests

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,21 @@
 """Здесь надо написать тесты с использованием unittest для модуля stack."""
+from src.stack import Node, Stack
+
+
+def test_node():
+    n1 = Node(5, None)
+    n2 = Node('a', n1)
+    assert n1.data == 5
+    assert n2.data == 'a'
+
+
+def test_steck():
+    stack = Stack()
+    stack.push('one')
+    stack.push('two')
+    stack.push('three')
+
+    assert stack.top.data == 'three'
+    assert stack.top.next_node.data == 'two'
+    assert stack.top.next_node.next_node.data == 'one'
+    assert stack.top.next_node.next_node.next_node is None
