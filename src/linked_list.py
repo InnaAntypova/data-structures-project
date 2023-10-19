7d14a7e3e468debe78bafeb429008105a8f738d9
Inna Antypova <nightallatiel@gmail.com>
2023-10-19 15:36:58 +0300

    add lilinked_list

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,18 +1,43 @@
 class Node:
     """Класс для узла односвязного списка"""
-    pass
+
+    def __init__(self, data, next_node=None):
+        self.data = data
+        self.next_node = next_node
 
 
 class LinkedList:
     """Класс для односвязного списка"""
 
+    def __init__(self):
+        self.head = None
+
     def insert_beginning(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в начало связанного списка"""
-        pass
+        node = Node(data)
+        node.data = data
+        if self.head is None:
+            self.head = node
+            return
+        else:
+            node.next_node = self.head
+            self.head = node
+            return
 
     def insert_at_end(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в конец связанного списка"""
-        pass
+        node = Node(data)
+        node.data = data
+        if self.head is None:
+            self.head = node
+            return
+        else:
+            temp_node = self.head
+            while temp_node.next_node:
+                temp_node = temp_node.next_node
+
+            temp_node.next_node = node
+            return
 
     def __str__(self) -> str:
         """Вывод данных односвязного списка в строковом представлении"""
