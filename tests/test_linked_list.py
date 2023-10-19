570d8c8a212758014b0bdb7d2c7873912537cebd
Inna Antypova <nightallatiel@gmail.com>
2023-10-19 20:22:21 +0300

    add tests

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -1,1 +1,24 @@
 """Здесь надо написать тесты с использованием unittest для модуля linked_list."""
+import pytest
+
+from src.linked_list import Node, LinkedList
+
+
+def test_insert_beginning():
+    link_list = LinkedList()
+
+    link_list.insert_beginning({'key_1': 'data1'})
+    link_list.insert_beginning({'key_2': 'data2'})
+    link_list.insert_beginning({'key_3': 'data3'})
+
+    assert str(link_list) == " {'key_3': 'data3'} -> {'key_2': 'data2'} -> {'key_1': 'data1'} -> None"
+
+
+def test_insert_at_end():
+    link_list = LinkedList()
+
+    link_list.insert_at_end({'key_1': 'data1'})
+    link_list.insert_at_end({'key_2': 'data2'})
+    link_list.insert_at_end({'key_3': 'data3'})
+
+    assert str(link_list) == " {'key_1': 'data1'} -> {'key_2': 'data2'} -> {'key_3': 'data3'} -> None"
